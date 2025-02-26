2ce11767da555d33d0a85f7cdc7b5a292b431d24
Guillaume <135722971+GuillaumeV28@users.noreply.github.com>
2025-02-26 08:59:11 +0100

    commit database
    Initialisation de la database

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'reservation_system';
+$user = 'root';
+$password = '';
+$charset = 'utf8mb4';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die('Erreur de connexion à la base de données : ' . $e->getMessage());
+}
+?>
